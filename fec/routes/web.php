30f8380debd6887833ecfd7bfb33fce49a2bafc4
Username-5129 <clasyrosi@gmail.com>
2025-05-28 17:08:47 +0300

    added authentication with Laravel Breeze (simple starter kit)

--- fec/routes/web.php
+++ fec/routes/web.php
@@ -1,13 +1,28 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\EncryptedFilesController;
 use App\Http\Controllers\AuthController;
 
-
 Route::get('/', [HomeController::class, 'index']);
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
 Route::get('/about', [AboutController::class, 'index']);
 Route::get('/files', [EncryptedFilesController::class, 'index']);
-Route::get('/login', [AuthController::class, 'index']);
+Route::get('/login1', [AuthController::class, 'index']);
