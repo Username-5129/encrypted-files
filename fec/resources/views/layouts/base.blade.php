fae59cde923577f20dd7a5eb0b962848f2615658
laims-reali <lg24062@edu.lu.lv>
2025-06-09 21:02:41 +0300

    reverting back to before authentication template was used

--- fec/resources/views/layouts/base.blade.php
+++ fec/resources/views/layouts/base.blade.php
@@ -3,20 +3,15 @@
 <head>
     <title>@yield('title', 'My Website')</title>
     <!-- Add your CSS and meta tags here -->
-    @vite('resources/css/app.css')
 </head>
 <body>
     <nav>
         <!-- Your navbar HTML here -->
-        <a href="/">Home 1</a>
+        <a href="/">Home</a>
         <a href="/about">About</a>
         <a href="/files">Files</a>
         <a href="/login">Login</a> <!-- This should be a link to the login page -->
     </nav>
-    <h1 class=" underline">
-        HEELO WORLD!
-</h1>
-    
     <div class="container">
         @yield('content')
     </div>
